c48c9bf01948b863b95c6bf2cd2a8b2b3b2b6622
Jimb Esser <wasteland@gmail.com>
2019-08-15 09:34:33 -0700

    Refactor server to client communication into separate file

--- src/server/glov/channel_server.js
+++ src/server/glov/channel_server.js
@@ -4,21 +4,14 @@
 const ack = require('../../common/ack.js');
 const assert = require('assert');
 const cmd_parse = require('../../common/cmd_parse.js');
-const client_worker = require('./client_worker.js');
 const { ChannelWorker } = require('./channel_worker.js');
+const client_comm = require('./client_comm.js');
 const default_workers = require('./default_workers.js');
 const exchange = require('./exchange.js');
 
 const { max } = Math;
 
-// Messages not allowed to be forwarded from client to arbitrary worker
-const RESERVED = {
-  'subscribe': 1, 'unsubscribe': 1,
-  'client_changed': 1,
-  'apply_channel_data': 1, 'set_channel_data': 1,
-};
-
-function logdata(data) {
+export function logdata(data) {
   if (data === undefined) {
     return '';
   }
@@ -104,114 +97,8 @@ export function channelServerSend(source, dest, msg, err, data, resp_func) {
   trySend();
 }
 
-function onUnSubscribe(channel_server, client, channel_id) {
-  client.client_channel.unsubscribeOther(channel_id);
-}
-
-function onClientDisconnect(channel_server, client) {
-  client.client_channel.unsubscribeAll();
-}
-
-function onSubscribe(channel_server, client, channel_id) {
-  client.client_channel.subscribeOther(channel_id);
-}
-
-function onSetChannelData(channel_server, client, data, resp_func) {
-  if (!data.q) {
-    console.log(`client_id:${client.id}->${data.channel_id}: set_channel_data ${logdata(data)}`);
-  }
-  data.key = String(data.key);
-  let channel_id = data.channel_id;
-  assert(channel_id);
-
-  let key = data.key.split('.');
-  if (key[0] !== 'public' && key[0] !== 'private') {
-    console.log(` - failed, invalid scope: ${key[0]}`);
-    resp_func('failed: invalid scope');
-    return;
-  }
-  if (!key[1]) {
-    console.log(' - failed, missing member name');
-    resp_func('failed: missing member name');
-    return;
-  }
-
-  // TODO: Disable autocreate for this call?
-  // TODO: Error if channel does not exist, but do not require an ack? channelServerSend needs a simple "sent" ack?
-  channelServerSend(client.client_channel, channel_id, 'set_channel_data', null, data);
-  resp_func();
-}
-
-function onChannelMsg(channel_server, client, data, resp_func) {
-  // Messages to everyone subscribed to the channel, e.g. chat
-  console.log(`client_id:${client.id}->${data.channel_id}: channel_msg ${logdata(data)}`);
-  if (RESERVED[data.msg]) {
-    return void resp_func(`Not allowed to send internal message ${data.msg}`);
-  }
-  let channel_id = data.channel_id;
-  assert(channel_id);
-  let client_channel = client.client_channel;
-
-  if (!client_channel.isSubscribedTo(channel_id)) {
-    return void resp_func(`Client is not on channel ${channel_id}`);
-  }
-  if (data.broadcast && typeof data.data !== 'object') {
-    return void resp_func('Broadcast requires data object');
-  }
-  if (!resp_func.expecting_response) {
-    resp_func = null;
-  }
-  if (data.broadcast) {
-    delete data.broadcast;
-    channelServerSend(client_channel, channel_id, 'broadcast', null, data, resp_func);
-  } else {
-    channelServerSend(client_channel, channel_id, data.msg, null, data.data, resp_func);
-  }
-}
-
-const regex_valid_username = /^[a-zA-Z0-9_]+$/u;
-function onLogin(channel_server, client, data, resp_func) {
-  console.log(`client_id:${client.id}->server login ${logdata(data)}`);
-  if (!data.name) {
-    return resp_func('invalid username');
-  }
-  if ({}[data.name]) {
-    // hasOwnProperty, etc
-    return resp_func('invalid username');
-  }
-  if (!data.name.match(regex_valid_username)) {
-    // has a "." or other invalid character
-    return resp_func('invalid username');
-  }
-
-  let client_channel = client.client_channel;
-  assert(client_channel);
-
-  return channelServerSend(client_channel, `user.${data.name}`, 'login', null, {
-    password: data.password,
-  }, function (err, resp_data) {
-    if (!err) {
-      client_channel.ids = client_channel.ids || {};
-      client_channel.ids.user_id = data.name;
-      client.ids.user_id = data.name;
-      client_channel.ids.display_name = resp_data.display_name;
-      client.ids.display_name = resp_data.display_name;
-
-      // Tell channels we have a new user id/display name
-      for (let channel_id in client_channel.subscribe_counts) {
-        channelServerSend(client_channel, channel_id, 'client_changed');
-      }
-
-      // Always subscribe client to own user
-      onSubscribe(channel_server, client, `user.${data.name}`);
-    }
-    resp_func(err);
-  });
-}
-
 class ChannelServer {
   constructor() {
-    this.last_client_id = 0;
     this.channel_types = {};
     this.local_channels = {};
   }
@@ -257,28 +144,9 @@ class ChannelServer {
   init(ds_store, ws_server) {
     this.ds_store = ds_store;
     this.ws_server = ws_server;
-    ws_server.on('client', (client) => {
-      assert(!client.ids);
-      client.ids = {
-        type: 'client',
-        id: this.clientIdFromWSClient(client),
-        // ws_client_id: client.id // not needed anymore?
-        user_id: null,
-        display_name: null,
-      };
-      client.client_channel = this.createChannelLocal(`client.${client.ids.id}`);
-      client.client_channel.client = client;
-      client.ids.channel_id = client.client_channel.channel_id;
-    });
-    ws_server.on('disconnect', onClientDisconnect.bind(this, this));
-    ws_server.onMsg('subscribe', onSubscribe.bind(this, this));
-    ws_server.onMsg('unsubscribe', onUnSubscribe.bind(this, this));
-    ws_server.onMsg('set_channel_data', onSetChannelData.bind(this, this));
-    ws_server.onMsg('channel_msg', onChannelMsg.bind(this, this));
-    ws_server.onMsg('login', onLogin.bind(this, this));
+    client_comm.init(this);
 
     default_workers.init(this);
-    client_worker.init(this);
 
     this.tick_func = this.doTick.bind(this);
     this.tick_time = 250;
