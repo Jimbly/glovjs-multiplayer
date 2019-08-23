5f7ff01295f748443866e15000cce516cec65c0b
Jimb Esser <wasteland@gmail.com>
2019-08-23 09:52:48 -0700

    Convert to better-babelifying classes

--- src/client/account_ui.js
+++ src/client/account_ui.js
@@ -6,98 +6,96 @@ const net = require('./glov/net.js');
 const ui = require('./glov/ui.js');
 const { vec4 } = require('./glov/vmath.js');
 
-class AccountUI {
-  constructor() {
-    this.edit_box_name = ui.createEditBox({
-      placeholder: 'Username',
-      initial_focus: true,
-      text: local_storage.get('name') || '',
-    });
-    this.edit_box_password = ui.createEditBox({
-      placeholder: 'Password',
-      type: 'password',
-      text: local_storage.get('name') && local_storage.get('password') || '',
-    });
-  }
-
-  showLogin(param) {
-    let { x, y, style, button_height, prelogout, center } = param;
-    button_height = button_height || ui.button_height;
-    let edit_box_name = this.edit_box_name;
-    let edit_box_password = this.edit_box_password;
-    let login_message;
-    const BOX_H = ui.font_height;
-    let pad = 10;
-    let min_h = BOX_H * 2 + pad * 3 + button_height;
-    let calign = center ? glov_font.ALIGN.HRIGHT : glov_font.ALIGN.HLEFT;
-    if (!net.client.connected) {
-      login_message = 'Establishing connection...';
-    } else if (net.subs.logging_in) {
-      login_message = 'Logging in...';
-    } else if (net.subs.logging_out) {
-      login_message = 'Logging out...';
-    } else if (!net.subs.loggedIn()) {
-      let submit = false;
-      let w = 100;
-      ui.font.drawSizedAligned(style, center ? x - 8 : x, y, Z.UI, ui.font_height, calign, 0, 0, 'Username:');
-      submit = edit_box_name.run({ x: center ? x : x + 140, y, w }) === edit_box_name.SUBMIT || submit;
-      y += BOX_H + pad;
-      ui.font.drawSizedAligned(style, center ? x - 8 : x, y, Z.UI, ui.font_height, calign, 0, 0, 'Password:');
-      submit = edit_box_password.run({ x: center ? x : x + 140, y, w }) === edit_box_password.SUBMIT || submit;
-      y += BOX_H + pad;
-      submit = ui.buttonText({
-        x, y, w: 240, h: button_height,
-        text: 'Log in/Create User',
-      }) || submit;
-      y += button_height + pad;
-
-      if (submit) {
-        local_storage.set('name', edit_box_name.text);
-        // do log in!
-        net.subs.login(edit_box_name.text, edit_box_password.text, function (err) {
-          if (err) {
-            ui.modalDialog({
-              title: 'Login Error',
-              text: err,
-              buttons: {
-                'OK': null,
-              },
-            });
-          }
-        });
-      }
-    } else {
-      let user_id = net.subs.loggedIn();
-      let user_channel = net.subs.getChannel(`user.${user_id}`);
-      let display_name = user_channel.getChannelData('public.display_name') || user_id;
-      ui.font.drawSizedAligned(style, center ? x - 8 : x + 240 + 8, y, Z.UI, ui.font_height,
-        // eslint-disable-next-line no-bitwise
-        calign | glov_font.ALIGN.VCENTER, 0, button_height,
-        (user_id !== display_name) ?
-          `Logged in as ${user_id} (Display Name: ${display_name})` :
-          `Logged in as ${user_id}`);
-      if (ui.buttonText({
-        x, y, w: 240, h: button_height,
-        text: 'Log out',
-      })) {
-        edit_box_password.setText('');
-        if (prelogout) {
-          prelogout();
-        }
-        net.subs.logout();
-      }
-      y += button_height + 8;
-    }
-    if (login_message) {
-      let w = ui.font.drawSizedAligned(style, x - 400, y, Z.UI, ui.font_height * 1.5, glov_font.ALIGN.HVCENTERFIT, 800,
-        min_h, login_message);
-      w += 100;
-      ui.drawRect(x - w / 2, y, x + w / 2, y + min_h, Z.UI - 0.5, vec4(0,0,0,0.25));
-      y += min_h;
-    }
-    return y;
-  }
+function AccountUI() {
+  this.edit_box_name = ui.createEditBox({
+    placeholder: 'Username',
+    initial_focus: true,
+    text: local_storage.get('name') || '',
+  });
+  this.edit_box_password = ui.createEditBox({
+    placeholder: 'Password',
+    type: 'password',
+    text: local_storage.get('name') && local_storage.get('password') || '',
+  });
 }
+
+AccountUI.prototype.showLogin = function (param) {
+  let { x, y, style, button_height, prelogout, center } = param;
+  button_height = button_height || ui.button_height;
+  let edit_box_name = this.edit_box_name;
+  let edit_box_password = this.edit_box_password;
+  let login_message;
+  const BOX_H = ui.font_height;
+  let pad = 10;
+  let min_h = BOX_H * 2 + pad * 3 + button_height;
+  let calign = center ? glov_font.ALIGN.HRIGHT : glov_font.ALIGN.HLEFT;
+  if (!net.client.connected) {
+    login_message = 'Establishing connection...';
+  } else if (net.subs.logging_in) {
+    login_message = 'Logging in...';
+  } else if (net.subs.logging_out) {
+    login_message = 'Logging out...';
+  } else if (!net.subs.loggedIn()) {
+    let submit = false;
+    let w = 100;
+    ui.font.drawSizedAligned(style, center ? x - 8 : x, y, Z.UI, ui.font_height, calign, 0, 0, 'Username:');
+    submit = edit_box_name.run({ x: center ? x : x + 140, y, w }) === edit_box_name.SUBMIT || submit;
+    y += BOX_H + pad;
+    ui.font.drawSizedAligned(style, center ? x - 8 : x, y, Z.UI, ui.font_height, calign, 0, 0, 'Password:');
+    submit = edit_box_password.run({ x: center ? x : x + 140, y, w }) === edit_box_password.SUBMIT || submit;
+    y += BOX_H + pad;
+    submit = ui.buttonText({
+      x, y, w: 240, h: button_height,
+      text: 'Log in/Create User',
+    }) || submit;
+    y += button_height + pad;
+
+    if (submit) {
+      local_storage.set('name', edit_box_name.text);
+      // do log in!
+      net.subs.login(edit_box_name.text, edit_box_password.text, function (err) {
+        if (err) {
+          ui.modalDialog({
+            title: 'Login Error',
+            text: err,
+            buttons: {
+              'OK': null,
+            },
+          });
+        }
+      });
+    }
+  } else {
+    let user_id = net.subs.loggedIn();
+    let user_channel = net.subs.getChannel(`user.${user_id}`);
+    let display_name = user_channel.getChannelData('public.display_name') || user_id;
+    ui.font.drawSizedAligned(style, center ? x - 8 : x + 240 + 8, y, Z.UI, ui.font_height,
+      // eslint-disable-next-line no-bitwise
+      calign | glov_font.ALIGN.VCENTER, 0, button_height,
+      (user_id !== display_name) ?
+        `Logged in as ${user_id} (Display Name: ${display_name})` :
+        `Logged in as ${user_id}`);
+    if (ui.buttonText({
+      x, y, w: 240, h: button_height,
+      text: 'Log out',
+    })) {
+      edit_box_password.setText('');
+      if (prelogout) {
+        prelogout();
+      }
+      net.subs.logout();
+    }
+    y += button_height + 8;
+  }
+  if (login_message) {
+    let w = ui.font.drawSizedAligned(style, x - 400, y, Z.UI, ui.font_height * 1.5, glov_font.ALIGN.HVCENTERFIT, 800,
+      min_h, login_message);
+    w += 100;
+    ui.drawRect(x - w / 2, y, x + w / 2, y + min_h, Z.UI - 0.5, vec4(0,0,0,0.25));
+    y += min_h;
+  }
+  return y;
+};
 
 export function create(...args) {
   return new AccountUI(...args);
--- src/client/chat_ui.js
+++ src/client/chat_ui.js
@@ -3,146 +3,181 @@
 const camera2d = require('./glov/camera2d.js');
 const glov_font = require('./glov/font.js');
 const input = require('./glov/input.js');
-const ui = require('./glov/ui.js');
 const net = require('./glov/net.js');
+const ui = require('./glov/ui.js');
 
-class ChatUI {
-  constructor(cmd_parse) {
-    this.cmd_parse = cmd_parse;
-    this.edit_text_entry = ui.createEditBox({
-      placeholder: 'Chatbox',
-      initial_focus: false,
-      text: '',
-    });
-    this.channel = null;
+function ChatUI(cmd_parse) {
+  this.cmd_parse = cmd_parse;
+  this.edit_text_entry = ui.createEditBox({
+    placeholder: 'Chatbox',
+    initial_focus: false,
+    text: '',
+  });
+  this.channel = null;
 
-    this.on_join = this.onMsgJoin.bind(this);
-    this.on_leave = this.onMsgLeave.bind(this);
-    this.on_chat = this.onMsgChat.bind(this);
-    this.msgs = [];
-    this.max_messages = 6;
+  this.on_join = this.onMsgJoin.bind(this);
+  this.on_leave = this.onMsgLeave.bind(this);
+  this.on_chat = this.onMsgChat.bind(this);
+  this.msgs = [];
+  this.max_messages = 6;
 
-    this.font_style = glov_font.style(null, {
-      color: 0xBBBBBBff,
-      outline_width: 1.0,
-      outline_color: 0x000000ff,
-    });
+  this.font_style = glov_font.style(null, {
+    color: 0xBBBBBBff,
+    outline_width: 1.0,
+    outline_color: 0x000000ff,
+  });
+}
 
+ChatUI.prototype.addChat = function (msg) {
+  this.msgs.push(msg);
+};
+ChatUI.prototype.onMsgJoin = function (data) {
+  this.addChat(`${data.display_name} joined the channel`);
+};
+ChatUI.prototype.onMsgLeave = function (data) {
+  this.addChat(`${data.display_name} left the channel`);
+};
+ChatUI.prototype.onMsgChat = function (data) {
+  this.addChat(`[${data.client_ids.display_name}] ${data.msg}`);
+};
+
+ChatUI.prototype.runLate = function () {
+  this.did_run_late = true;
+  if (input.keyDownEdge(input.KEYS.RETURN)) {
+    this.edit_text_entry.focus();
+  }
+  if (input.keyDownEdge(input.KEYS.SLASH) ||
+    input.keyDownEdge(input.KEYS.NUMPAD_DIVIDE)
+  ) {
+    this.edit_text_entry.focus();
+    this.edit_text_entry.setText('/');
+  }
+};
+
+ChatUI.prototype.run = function (opts) {
+  opts = opts || {};
+  if (net.client.disconnected) {
+    ui.font.drawSizedAligned(
+      glov_font.style(null, {
+        outline_width: 2,
+        outline_color: 0x000000ff,
+        color: 0xDD2020ff
+      }),
+      camera2d.x0(), camera2d.y0(), Z.DEBUG,
+      ui.font_height, glov_font.ALIGN.HVCENTER, camera2d.w(), camera2d.h() * 0.20,
+      `Connection lost, attempting to reconnect (${(net.client.timeSinceDisconnect()/1000).toFixed(0)})...`);
   }
 
-  addChat(msg) {
-    this.msgs.push(msg);
+  if (!this.did_run_late) {
+    this.runLate();
   }
-  onMsgJoin(data) {
-    this.addChat(`${data.display_name} joined the channel`);
-  }
-  onMsgLeave(data) {
-    this.addChat(`${data.display_name} left the channel`);
-  }
-  onMsgChat(data) {
-    this.addChat(`[${data.client_ids.display_name}] ${data.msg}`);
-  }
-
-  runLate() {
-    this.did_run_late = true;
-    if (input.keyDownEdge(input.KEYS.RETURN)) {
-      this.edit_text_entry.focus();
-    }
-    if (input.keyDownEdge(input.KEYS.SLASH) ||
-      input.keyDownEdge(input.KEYS.NUMPAD_DIVIDE)
-    ) {
-      this.edit_text_entry.focus();
-      this.edit_text_entry.setText('/');
-    }
-  }
-
-  run() {
-    if (net.client.disconnected) {
-      ui.font.drawSizedAligned(
-        glov_font.style(null, {
-          outline_width: 2,
-          outline_color: 0x000000ff,
-          color: 0xDD2020ff
-        }),
-        camera2d.x0(), camera2d.y0(), Z.DEBUG,
-        ui.font_height, glov_font.ALIGN.HVCENTER, camera2d.w(), camera2d.h() * 0.20,
-        `Connection lost, attempting to reconnect (${(net.client.timeSinceDisconnect()/1000).toFixed(0)})...`);
-    }
-
-    if (!this.did_run_late) {
-      this.runLate();
-    }
-    this.did_run_late = false;
-    let x = camera2d.x0() + 10;
-    let y0 = camera2d.y1();
-    let y = y0;
-    let w = (camera2d.x1() - camera2d.x0()) / 2;
-    if (net.subs.loggedIn() && !(ui.modal_dialog || ui.menu_up)) {
-      y -= 40;
-      if (this.edit_text_entry.run({ x, y, w }) === this.edit_text_entry.SUBMIT) {
-        let text = this.edit_text_entry.getText();
-        if (text) {
-          if (text[0] === '/') {
-            let handleCmdParse = (err, resp) => {
-              if (err) {
-                this.addChat(`[error] ${err}`);
-              } else if (resp) {
-                this.addChat(`[system] ${(typeof resp === 'string') ? resp : JSON.stringify(resp)}`);
-              }
-            };
-            let command = text.slice(1);
-            this.cmd_parse.handle(null, command, (err, resp) => {
-              if (err && this.cmd_parse.was_not_found) {
-                // forward to server
-                net.subs.sendCmdParse(command, handleCmdParse);
-              } else {
-                handleCmdParse(err, resp);
-              }
-            });
-          } else {
-            this.channel.send('chat', { msg: text }, { broadcast: true });
-          }
-          this.edit_text_entry.setText('');
+  this.did_run_late = false;
+  let x = camera2d.x0() + 10;
+  let y0 = camera2d.y1();
+  let y = y0;
+  let w = (camera2d.x1() - camera2d.x0()) / 2;
+  if (net.subs.loggedIn() && !(ui.modal_dialog || ui.menu_up || opts.hide)) {
+    y -= 40;
+    let was_focused = this.edit_text_entry.isFocused();
+    let res = this.edit_text_entry.run({ x, y, w });
+    let is_focused = this.edit_text_entry.isFocused();
+    if (res === this.edit_text_entry.SUBMIT) {
+      let text = this.edit_text_entry.getText();
+      if (text) {
+        if (text[0] === '/') {
+          let handleCmdParse = (err, resp) => {
+            if (err) {
+              this.addChat(`[error] ${err}`);
+            } else if (resp) {
+              this.addChat(`[system] ${(typeof resp === 'string') ? resp : JSON.stringify(resp)}`);
+            }
+          };
+          let command = text.slice(1);
+          this.cmd_parse.handle(null, command, (err, resp) => {
+            if (err && this.cmd_parse.was_not_found) {
+              // forward to server
+              net.subs.sendCmdParse(command, handleCmdParse);
+            } else {
+              handleCmdParse(err, resp);
+            }
+          });
         } else {
-          ui.focusCanvas();
+          this.channel.send('chat', { msg: text }, { broadcast: true });
         }
+        this.edit_text_entry.setText('');
+      } else {
+        is_focused = false;
+        ui.focusCanvas();
       }
     }
-    y -= 8;
-    let numlines;
-    let indent = 80;
-    function wordCallback(ignored, linenum, word) {
-      numlines = Math.max(numlines, linenum);
+    if (opts.pointerlock && was_focused && !is_focused && !input.pointerLocked()) {
+      // Lost focus, restore pointer lock
+      input.pointerLockEnter(true);
     }
-    for (let ii = 0; ii < Math.min(this.msgs.length, this.max_messages); ++ii) {
-      let line = this.msgs[this.msgs.length - ii - 1];
-      numlines = 0;
-      ui.font.wrapLines(w, indent, ui.font_height, line, wordCallback);
-      y -= ui.font_height * (numlines + 1);
-      ui.font.drawSizedWrapped(this.font_style, x, y, Z.CHAT + 1, w, indent, ui.font_height, line);
+    if (opts.pointerlock && is_focused && input.pointerLocked()) {
+      // Gained focus undo pointerlock
+      input.pointerLockExit();
     }
-
-    let border = 8;
-    ui.drawRect(x - border, y - border, x + w + border + 8, y0, Z.CHAT, [1,1,1,0.2]);
+    if (is_focused && was_focused && input.mouseDownEdge({ peek: true })) {
+      // On touch, tapping doesn't always remove focus from the edit box!
+      // Maybe this logic should be in the editbox logic?
+      ui.focusCanvas();
+    }
+  }
+  y -= 8;
+  let numlines;
+  let indent = 80;
+  function wordCallback(ignored, linenum, word) {
+    numlines = Math.max(numlines, linenum);
+  }
+  for (let ii = 0; ii < Math.min(this.msgs.length, this.max_messages); ++ii) {
+    let line = this.msgs[this.msgs.length - ii - 1];
+    numlines = 0;
+    ui.font.wrapLines(w, indent, ui.font_height, line, wordCallback);
+    y -= ui.font_height * (numlines + 1);
+    ui.font.drawSizedWrapped(this.font_style, x, y, Z.CHAT + 1, w, indent, ui.font_height, line);
   }
 
-  setChannel(channel) {
-    if (channel === this.channel) {
-      return;
-    }
-    if (this.channel) {
-      this.addChat(`Left channel ${this.channel.channel_id}`);
-      this.channel.removeMsgHandler('chat', this.on_chat);
-      this.channel.removeMsgHandler('join', this.on_join);
-      this.channel.removeMsgHandler('leave', this.on_leave);
-    }
-    this.channel = channel;
+  let border = 8;
+  ui.drawRect(x - border, y - border, x + w + border + 8, y0, Z.CHAT, [1,1,1,0.2]);
+};
+
+ChatUI.prototype.setChannel = function (channel) {
+  if (channel === this.channel) {
+    return;
+  }
+  if (this.channel) {
+    this.addChat(`Left channel ${this.channel.channel_id}`);
+    this.channel.removeMsgHandler('chat', this.on_chat);
+    this.channel.removeMsgHandler('join', this.on_join);
+    this.channel.removeMsgHandler('leave', this.on_leave);
+  }
+  this.channel = channel;
+  if (this.channel) {
     channel.onMsg('chat', this.on_chat);
     channel.onMsg('join', this.on_join);
     channel.onMsg('leave', this.on_leave);
     this.addChat(`Joined channel ${this.channel.channel_id}`);
+    channel.onceSubscribe((data) => {
+      let clients = data && data.public && data.public.clients;
+      if (clients) {
+        let here = [];
+        for (let client_id in clients) {
+          if (client_id === net.client.id) {
+            continue;
+          }
+          let client = clients[client_id];
+          if (client.ids) {
+            here.push(client.ids.display_name || client.ids.user_id || client_id);
+          }
+        }
+        if (here.length) {
+          this.addChat(`Other users already here: ${here.join(', ')}`);
+        }
+      }
+    });
   }
-}
+};
 
 export function create(...args) {
   return new ChatUI(...args);
