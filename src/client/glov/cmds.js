3715dd188de73bbf45d1bead08244a2be4108041
Jimb Esser <wasteland@gmail.com>
2020-01-06 12:37:13 -0800

    Add /help command and custom labels for settings

--- src/client/glov/cmds.js
+++ src/client/glov/cmds.js
@@ -70,3 +70,25 @@ cmd_parse.register({
     resp_func(null, `Renderer=WebGL${engine.webgl2?2:1}`);
   }
 });
+
+function cmdDesc(cmd_data) {
+  return `/${cmd_data.cmd} - ${cmd_data.help}`;
+}
+
+cmd_parse.register({
+  cmd: 'help',
+  help: 'Searches commands',
+  func: function (str, resp_func) {
+    let list = cmd_parse.autoComplete('', this && this.access);
+    if (str) {
+      let str_cname = cmd_parse.canonical(str);
+      let str_lc = str.toLowerCase();
+      list = list.filter((cmd_data) => cmd_data.cname.indexOf(str_cname) !== -1 ||
+          cmd_data.help.toLowerCase().indexOf(str_lc) !== -1);
+    }
+    if (!list.length) {
+      return void resp_func(null, `No commands found matching "${str}"`);
+    }
+    resp_func(null, list.map(cmdDesc).join('\n'));
+  }
+});
